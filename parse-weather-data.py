cc362ed2afdf6afdf786c225432e92e527caf0ad
Toni Räsänen <toni.rasanen@iki.fi>
2018-10-11 18:41:55 +0300

    cleaning and comments

--- parse-weather-data.py
+++ parse-weather-data.py
@@ -3,46 +3,61 @@ import pandas as pd
 import numpy as np
 import csv
 
-# parse weather csv to same form as electricity data
-# 00:50 --> 01:00
-# 01:20 01:50 --> 02:00 (count average of two temps)
-# drop other clumns than data time and temperature
+'''
+  parse weather csv to same form as electricity data
+  00:50 --> 01:00
+  01:20 + 01:50 --> 02:00 (count average of the values)
+  drop other columns than 'DateTime', 'T', 'P', 'U', 'Ff' and 'Td'
+'''
 
+# the destination file created from from the source data
+OUTPUT_CSV_FILE = 'weather_data_final.csv'
 
-# read csv 
+# read the "raw" source data 
 weather_data_frame = pd.read_csv('weather-Rome_mod.csv')
 
 
 print('hello')
 
-# Advance hour by one and reset the minutes
-# 23:xx -> 00:00
-# 22:xx -> 23:00
-# 21:xx -> 22:00
-#   ...
-# 00:xx -> 01:00
+
 def round_the_clock(date_time_str):
+    '''
+    Advance hour by one and reset the minutes
+        23:xx -> 00:00
+        22:xx -> 23:00
+        21:xx -> 22:00
+        ...
+        00:xx -> 01:00
+     '''
     new_time_str = ''
     # 18.05.2018 22:20
     if date_time_str[-5:-3] == '23':
         new_time_str = date_time_str[:11] + '00:00'
     else:
-        new_hour = int(float(date_time_str[-5:-3])) + 1
-        new_time_str = date_time_str[:11] + str(new_hour) + ':00'
+        new_hour = str(int(float(date_time_str[-5:-3])) + 1)
+        if len(new_hour) == 1:
+            new_hour = '0' + new_hour
+        new_time_str = date_time_str[:11] + new_hour + ':00'
     #print('new time new time: ', new_time_str)
     return new_time_str
 
 
 def write_to_new_csv(dataframe): 
-    with open('weather_data_final.csv', 'w') as csvfile:
+    '''
+        Create new csv file from the given dataframe.
+        The source data contains two weather entries per hour, merge these two
+        into a one row by counting averages for the values and resetting the time.
+    '''
+
+    with open(OUTPUT_CSV_FILE, 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)                     
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
         filewriter.writerow(['DateTime', 'T', 'P', 'U', 'Ff', 'Td'])    
 
         buffer_row_arr = []
         for index, row in dataframe.iterrows():
-            #filewriter.writerow(dataframe.iat[index,0].split(';'))
-            #print(row)
+
             row_value_array = row[0].split(';')
             
             if row_value_array[0][-2:] == '50':
@@ -67,7 +82,7 @@ def write_to_new_csv(dataframe):
 
         
         
-sub_data_frame = weather_data_frame.head(47) # one day
+sub_data_frame = weather_data_frame.head(10 * 47) # n days
 print(sub_data_frame)
 write_to_new_csv(sub_data_frame)
 
