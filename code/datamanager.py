dc116011a4c9f44ee31cc445026e6fc49fcff0e1
dennisn <denonjugush@gmail.com>
2018-10-15 18:04:26 +0300

    Created a new weather dataset

---
+++ code/datamanager.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""
+This module provides access methods to datasets.
+
+The files are stored locally and there could be some hardcoded paths
+"""
+
+
+import pandas as pd
+
+
+def get_weather_data():
+    """
+    Pull weather data.
+
+    Pulls data from the local directory and returns a dataframe
+    """
+    data = pd.io.parsers.read_csv('../weather_data_final.csv',
+                                  index_col=0, parse_dates=True).sort_index()
+    print('Weather Data Set\n{}'.format(data.head()))
+    return data
