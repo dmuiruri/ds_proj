e77d119f2c18f39a07fafdb27c3ff63eed782967
Toni Räsänen <toni.rasanen@iki.fi>
2018-10-24 22:28:42 +0300

    POST with csv files

--- code_files/api_client.py
+++ code_files/api_client.py
@@ -1,7 +1,10 @@
-
 import requests # http://docs.python-requests.org/en/master/
-import json     # https://docs.python.org/3/library/json.html
 
+'''
+
+(Test) client script for electricity consumption forecast API
+
+'''
 
 
 PREDICT_API_URL = 'http://127.0.0.1:5000/predict'
@@ -11,16 +14,16 @@ CONSUMPTION_FILE = '../data/energy_industrial.csv'
 WEATHER_FILE = '../data/hourly_resampled_weather_data.csv'
 
 
-# construct a payload for the prediction POST request
-# POST contains two files: consumption data and weather data
-#forecast_base_data = [('files', open(CONSUMPTION_FILE, 'rb')), ('files', open(WEATHER_FILE, 'rb'))]
+# # construct a payload for the prediction POST request
+# # POST contains two files: consumption data and weather data
+forecast_base_data = [('files', open(CONSUMPTION_FILE, 'rb')), ('files', open(WEATHER_FILE, 'rb'))]
 
 # make the POST 
-# TODO: this fails... payload too big?
-#response = requests.post(PREDICT_API_URL, files=forecast_base_data)
+response = requests.post(PREDICT_API_URL, files=forecast_base_data)
 
-# GET request for a sanity check
-response = requests.get(HELLO_API_URL)
+# GET requests for a sanity check
+#response = requests.get(HELLO_API_URL)
+#response = requests.get(PREDICT_API_URL)
 
 # server response
 print('Response from server:', response.text)
